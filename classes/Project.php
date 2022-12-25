0b6dcde873d1f66fece2ca5de90f7630f8496803
afsar-uddin <afsarwebdev@gmail.com>
2022-12-25 11:08:09 +0600

    username availabiltiy checked in database

--- classes/Project.php
+++ classes/Project.php
@@ -8,6 +8,23 @@ class Project{
 		$this->db = new Database();
 	}
 
+	public function checkusername($username) {
+		$query = "SELECT * FROM tbl_user WHERE username = '$username'";
+		$getUser = $this->db->select($query);
+
+		if($username == "") {
+			echo '<span class="error">Enter username please!</span>';
+			exit();
+		} elseif($getUser) {
+			echo "<span class='error'><b>$username</b> not available.</span>";
+			exit();	
+		} else {
+			echo "<span class='success'><b>$username</b> username is available.</span>";
+			exit();	
+		}
+ 
+	}
+
 	
 }
 ?>
